28b67c5ffe54ec9422618c32bb354ad370e9fc90
JaeSang Lee <52541784+Nevation@users.noreply.github.com>
2025-05-09 00:43:00 +0900

    fix: deploy 버그 수정 (#15)
    * fix: Terraform Cloud로 대체
    
    * fix: fe deploy.sh 수정

--- apps/web/deploy.sh
+++ apps/web/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # AWS CLI를 통해 s3에 빌드된걸 업로드한 이후 CloudFront 캐시 무효화 진행
-aws s3 cp ./dist/apps/web s3://lia-web-build --recursive
-aws cloudfront create-invalidation --distribution-id E20250508000000 --paths "/*"
+aws s3 cp ./apps/web/dist s3://lia-react-site --recursive
+aws cloudfront create-invalidation --distribution-id E1W0GS0EVJD0UE --paths "/*"
