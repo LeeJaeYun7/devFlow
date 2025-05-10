af2b3a45d8d9b5bd7fb2b6157227abf40189a930
JaeSang Lee <52541784+Nevation@users.noreply.github.com>
2025-05-10 19:44:49 +0900

    fix: VPC EP 추가 (#21)
    fix: vpc ep 추가

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -4,3 +4,24 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   tags = { Name = "main" }
 }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-northeast-2.ssm"
+  subnet_ids         = aws_subnet.private[*].id
+  security_group_ids = [aws_security_group.api_ecs.id]
+}
+
+resource "aws_vpc_endpoint" "ssm_messages" {
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-northeast-2.ssmmessages"
+  subnet_ids         = aws_subnet.private[*].id
+  security_group_ids = [aws_security_group.api_ecs.id]
+}
+
+resource "aws_vpc_endpoint" "ec2_messages" {
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-northeast-2.ec2messages"
+  subnet_ids         = aws_subnet.private[*].id
+  security_group_ids = [aws_security_group.api_ecs.id]
+}
