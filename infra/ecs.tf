b697e6c55df2d4f10d416f653fe9b5b976b2e618
JaeSang Lee <52541784+Nevation@users.noreply.github.com>
2025-05-18 17:15:24 +0900

    fix: ECS HealthCheck 로직 변경 (#32)
    fix: curl이 없어서 wget으로 변경

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -32,7 +32,7 @@ resource "aws_ecs_task_definition" "api" {
         }
       }
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:4600/api/health-check || exit 1"]
+        command     = ["CMD-SHELL", "wget -q --spider http://localhost:4600/api/health-check || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
