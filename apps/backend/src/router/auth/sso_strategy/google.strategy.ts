f10df3d3a24694431add2b8a9ea73a67b7294813
JaeSang Lee <52541784+Nevation@users.noreply.github.com>
2025-05-18 18:17:57 +0900

    fix: sso url 변경 (#36)

--- apps/backend/src/router/auth/sso_strategy/google.strategy.ts
+++ apps/backend/src/router/auth/sso_strategy/google.strategy.ts
@@ -10,7 +10,7 @@ import { AuthSsoMap } from '@lia/api/auth/auth.constant';
 export class SsoGoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private readonly configService: ConfigService) {
     const isProduction = configService.get('NODE_ENV') === 'production';
-    const baseUrl = isProduction ? 'https://api.lia.io' : 'http://localhost:4600';
+    const baseUrl = isProduction ? 'https://api.asklia.io' : 'http://localhost:4600';
     super({
       clientID: configService.get('GOOGLE_OAUTH_CLIENT_ID') ?? 'default',
       clientSecret: configService.get('GOOGLE_OAUTH_CLIENT_SECRET') ?? '',
