1865025be75b297f9378bc7993185ae258e95b6c
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2025-06-01 02:03:10 +0900

    fix: deploy.sh 수정  (#84)
    * Chat title 업데이트 시 오류 fix
    
    * Naver 크롤링 이슈 fix
    
    * 주석 복구
    
    * backend, data-collector Dockerfile 수정
    
    * pupeteer executablePath 수정
    
    * pupeteer 설정 변경
    
    * puppeteer executablePath 수정
    
    * deploy.sh 파일 수정
    
    ---------
    
    Co-authored-by: LeeJongbokZ <s2feeling@naver.com>

--- apps/backend/deploy.sh
+++ apps/backend/deploy.sh
@@ -4,6 +4,7 @@ REPO=731219764596.dkr.ecr.ap-northeast-2.amazonaws.com/backend
 SHA_TAG=$(git rev-parse --short HEAD)
 
 docker buildx build \
+  --progress=plain \
   --cache-from=type=local,src=/tmp/.buildx-cache \
   --cache-to=type=local,dest=/tmp/.buildx-cache \
   -t ${REPO}:latest \
