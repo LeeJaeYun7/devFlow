83d539dfc71830795c758fd49ed6b66774832337
Nevation <j209446@naver.com>
2025-05-31 13:19:51 +0900

    feat: admin deploy 구현

--- apps/admin/deploy.sh
+++ apps/admin/deploy.sh
@@ -2,4 +2,4 @@
 
 # AWS CLI를 통해 s3에 빌드된걸 업로드한 이후 CloudFront 캐시 무효화 진행
 aws s3 cp ./apps/admin/dist s3://lia-admin-site --recursive
-aws cloudfront create-invalidation --distribution-id E1W0GS0EVJD0UE --paths "/*"
+aws cloudfront create-invalidation --distribution-id E38H8DXFQMEHV --paths "/*"
