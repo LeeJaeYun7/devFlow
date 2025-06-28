8dee1de38ea704b9d0c1db0788601fb3c837fa7c
LeeJongbokZ <s2feeling@naver.com>
2025-06-28 22:34:11 +0900

    deploy.sh 수정

--- apps/backend/deploy.sh
+++ apps/backend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO=058264441782.dkr.ecr.ap-northeast-2.amazonaws.com/devflow
-SHA_TAG=$(git rev-parse --short HEAD)
+SHA_TAG=$GITHUB_SHA
 
 docker buildx build \
   --progress=plain \
